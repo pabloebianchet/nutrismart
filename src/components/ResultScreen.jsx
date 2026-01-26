209a3a378de85271eb10c84ee4493f35cb28c5ab
pabloebianchet <105605755+pablobianchet@users.noreply.github.com>
2026-01-26 20:04:18 -0300

    nueva subida

--- src/components/ResultScreen.jsx
+++ src/components/ResultScreen.jsx
@@ -2,7 +2,6 @@ import { Box, Typography, CircularProgress, LinearProgress, Paper } from "@mui/m
 import { useNutrition } from "../context/NutritionContext";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { CircularProgress, Box, Typography } from "@mui/material";
 
 const CircularScore = ({ value }) => {
   const getColor = (value) => {
