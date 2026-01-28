47e00eaa1627aebe8eb641aa6a5932e82453d1a8
Pablo Bianchet Martinez <166576628+pabloebianchet@users.noreply.github.com>
2026-01-28 17:42:35 -0300

    Add desktop navigation links to header

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,27 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserDataPage from "./pages/UserDataPage";
 import CapturePage from "./pages/CapturePage";
 import ResultScreen from "./components/ResultScreen";
+import AppHeader from "./components/AppHeader";
+import AboutPage from "./pages/AboutPage";
+import HowItWorksPage from "./pages/HowItWorksPage";
+import ContactPage from "./pages/ContactPage";
 
 
 const App = () => {
   return (
     <Router>
+      <AppHeader />
       <Routes>
-      
-          <Route path="/" element={<UserDataPage />} />
-          <Route path="/capture" element={<CapturePage />} />
-          <Route path="/result" element={<ResultScreen />} />
-       
+        <Route path="/" element={<UserDataPage />} />
+        <Route path="/capture" element={<CapturePage />} />
+        <Route path="/result" element={<ResultScreen />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/how-it-works" element={<HowItWorksPage />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </Router>
   );
 };
 
 export default App;
-
 
