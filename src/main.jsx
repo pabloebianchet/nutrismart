b083e2c57c43fa84e70d400704698304116bf0b2
pabloebianchet <105605755+pablobianchet@users.noreply.github.com>
2026-01-28 16:51:56 -0300

    google AUth

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { NutritionProvider } from "./context/NutritionContext";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <NutritionProvider>
-    <App />
-  </NutritionProvider>,
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <NutritionProvider>
+      <App />
+    </NutritionProvider>
+  </GoogleOAuthProvider>
 );
-
