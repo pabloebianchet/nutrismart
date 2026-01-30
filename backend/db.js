101ed24b460d5e69811e2f646965e6584c3012ba
pabloebianchet <105605755+pablobianchet@users.noreply.github.com>
2026-01-29 23:46:35 -0300

    Log mongo uri on startup

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,24 @@
+// import mongoose from "mongoose";
+
+// export const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log("MongoDB conectado");
+//   } catch (err) {
+//     console.error("Error MongoDB:", err.message);
+//     process.exit(1);
+//   }
+// };
+
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
+    console.log("🧪 MONGO_URI EN USO:", process.env.MONGO_URI);
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB conectado");
-  } catch (err) {
-    console.error("Error MongoDB:", err.message);
+    console.log("✅ MongoDB connected");
+  } catch (error) {
+    console.error("❌ MongoDB error:", error);
     process.exit(1);
   }
 };
