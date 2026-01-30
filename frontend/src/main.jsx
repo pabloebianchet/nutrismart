259540bacd2f73c073f0edb268cbbe7e54bf20a7
pabloebianchet <105605755+pablobianchet@users.noreply.github.com>
2026-01-29 22:10:17 -0300

    Fix App import extension for Vite build

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 import { NutritionProvider } from "./context/NutritionContext";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./index.css";
