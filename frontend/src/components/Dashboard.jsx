0fb16d357ec11ad21a0053e0da8cd825bdd75c4a
pabloebianchet <105605755+pablobianchet@users.noreply.github.com>
2026-02-04 15:17:41 -0300

    skeleton 2

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -197,7 +197,7 @@ const Dashboard = () => {
   /* ======================
      Render
   ====================== */
-  if (loadingUserData) {
+  if (!userData || loading) {
     return <DashboardSkeleton />;
   }
 
